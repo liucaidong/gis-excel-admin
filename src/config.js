1b642af27f436b1a18b0592e4af166967a71d01d
liucaidong <791136794@qq.com>
2019-12-26 03:32:22 +0800

    aaa

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 // const backendUrl = "http://localhost:8088"
-const backendUrl = "http://39.100.116.13:8088"
+// const backendUrl = "http://39.100.116.13:8088"
+const backendUrl = "http://101.200.82.96:8088"
 
 export default backendUrl
